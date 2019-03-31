5a9c1f45ff6eefdd35cbe459032f992c67ccdeaa
bangtiray <bangtiray@gmail.com>
2019-03-31 23:10:06 +0700

    buat indri

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        die('Halaman User');
+    }
+}
